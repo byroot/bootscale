d1c4f8eff3cae862a6c7839e61aec5952b266236
Alexey Shein <alexey.shein.dev@gmail.com>
2016-06-26 15:53:19 +0500

    Fix race condition in FileStorage
    There's a race condition when Bootscale tries to write serialized data
    on disk when it's run in highly concurrent environment, for example, via
    parallel_test command with big number of processes, or when many
    workers starting in cold boot in production.
    
    This commit brings `atomic_write` implementation from Sprockets
    library to Bootscale. First, it writes to a temporary file with unique
    name per thread, and renames file to destination when write is
    completed.

--- spec/file_storage_spec.rb
+++ spec/file_storage_spec.rb
@@ -4,7 +4,7 @@ require 'bootscale/file_storage'
 RSpec.describe Bootscale::FileStorage do
   around { |test| in_tmpdir(&test) }
 
-  subject { Bootscale::FileStorage.new('tmp/bootscale') }
+  subject { Bootscale::FileStorage.new(Dir.pwd) }
 
   it "dumps via Marshal by default" do
     expect(Bootscale::FileStorage::Serializer).to eq Marshal
@@ -14,5 +14,19 @@ RSpec.describe Bootscale::FileStorage do
     hash = {"\xDE" => 'utf-8'}
     subject.dump(%w(foo), hash)
     expect(subject.load(%w(foo))).to be == hash
+  end
+
+  context 'concurrent' do
+    it 'should not read garbage' do
+      load_path = %w[foo].freeze
+      expect do
+        threads = 100.times.map do |i|
+          Thread.new(subject) do |storage|
+            i.odd? ? storage.load(load_path) : storage.dump(load_path, 'a' * 100_000)
+          end
+        end
+        threads.map(&:join)
+      end.not_to raise_error
+    end
   end
 end
